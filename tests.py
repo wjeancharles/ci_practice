dce82f98db5620a9e408ab91756ce1c8b94a5ad6
wjeancharles <76965703+wjeancharles@users.noreply.github.com>
2023-02-26 17:19:38 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -12,7 +12,7 @@ class TestCase(unittest.TestCase):
     def test1(self):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
-        
+
     def test2(self):
         expected = "Hola World"
         self.assertEqual(task.my_func(), expected)
