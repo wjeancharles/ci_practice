0a64a77731820097ed9a22a412b93fd76980ca91
wjeancharles <76965703+wjeancharles@users.noreply.github.com>
2023-02-26 15:22:58 -0500

    Update tests.py

--- .github/workflows/tests.py
+++ .github/workflows/tests.py
@@ -8,11 +8,11 @@ import task
 
 
 class TestCase(unittest.TestCase):
-    
+
     def test1(self):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
-        
+
 
 if __name__ == '__main__':
     unittest.main()
